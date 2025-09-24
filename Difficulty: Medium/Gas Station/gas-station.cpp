77b0117d6ca655796e69819f3feaa537774bb777
Sanket-Kawale <sanketkawale101@gmail.com>
2025-09-24 18:26:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -1,22 +1,22 @@
 class Solution {
   public:
     int startStation(vector<int> &gas, vector<int> &cost) {
-        int deficit = 0;            //required balance
-        int balance = 0;            
-        int start = 0;
-        for(int i=0; i<gas.size(); i++){        //length of gas and cost vect is same
-            balance += gas[i] - cost[i];        //finding balance
-            if(balance < 0){                    //for negative balance
-                deficit += balance;             //update deficit
-                start = i+1;                    //update start to optimize space
-                balance = 0;                    //update balance
+        int deficit = 0;
+        int balance = 0;
+        int start  = 0;
+        for(int i=0; i<cost.size(); i++){
+            balance += gas[i] - cost[i];
+            if(balance < 0){
+                deficit += balance;
+                start = i+1;
+                balance = 0;
             }
         }
-        if(deficit + balance >= 0){             //cycle completed
-            return start;
-        }
-        else{
-            return -1;
+        if(deficit + balance >= 0){
+                return start;
+            }
+            else{
+                return -1;
         }
     }
 };
