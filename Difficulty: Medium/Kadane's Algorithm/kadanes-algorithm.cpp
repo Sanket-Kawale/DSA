e7a39aefb996540386cc39fade6fbf1845227f6b
Sanket-Kawale <sanketkawale101@gmail.com>
2025-10-13 19:13:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -4,9 +4,8 @@ class Solution {
         int n = arr.size();
         int target = INT_MIN;
         int temp = 0;
-        
         for(int i=0; i<n; i++){
-            temp += arr[i];
+            temp = temp + arr[i];
             
             if(temp > target){
                 target = temp;
@@ -14,7 +13,6 @@ class Solution {
             if(temp < 0){
                 temp = 0;
             }
-            
         }
         return target;
     }
