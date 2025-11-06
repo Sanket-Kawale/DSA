15c7e35b30fcfad64ce52b80cb106f85ef8ebbd1
Sanket-Kawale <sanketkawale101@gmail.com>
2025-11-06 20:09:03 +0530

    Time: 0 ms (100.00%) | Memory: 35.4 MB (61.57%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -15,14 +15,15 @@ public:
         if(root == NULL){
             return NULL;
         }
-        if(root->val == val){
+        if(root -> val == val){
             return root;
         }
-        if(root->val > val){
-            return searchBST(root->left, val);
-        }
-        else{
+        if(root->val < val){
             return searchBST(root->right, val);
         }
+        else{
+            return searchBST(root->left, val);
+        }
+        return root;
     }
 };
