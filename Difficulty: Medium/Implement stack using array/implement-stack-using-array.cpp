6fd6f8239d384ca7c20afb8203eb693458438e46
Sanket-Kawale <sanketkawale101@gmail.com>
2025-12-01 19:32:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement stack using array/implement-stack-using-array.cpp
@@ -0,0 +1,41 @@
+class myStack {
+  public:
+    int *arr;
+    int top;
+    int n;
+    myStack(int n) {
+        top = -1;
+        this->n = n;
+        arr = new int[n];
+    }
+
+    bool isEmpty() {
+        if(top == -1)   return 1;
+        else return 0;
+    }
+
+    bool isFull() {
+        if(top == n-1) return 1;
+        else return 0;
+    }
+
+    void push(int x) {
+        if(!isFull()){
+            top++;
+            arr[top] = x;
+        }
+    }
+
+    void pop() {
+        if(!isEmpty()){
+            top--;
+        }
+    }
+
+    int peek() {
+        if(top >=0){
+            return arr[top];
+        }
+        else return -1;
+    }
+};
