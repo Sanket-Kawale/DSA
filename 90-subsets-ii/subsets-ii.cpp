b151bfb7781db29008b9c1a16cd91751016b8b73
Sanket-Kawale <sanketkawale101@gmail.com>
2026-08-05 20:39:34 +0530

    Time: 3 ms (31.47%) | Memory: 17.5 MB (5.15%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>& ans, vector<int> nums, vector<int> output, int i){
+        if(i == nums.size()){
+            ans.push_back(output);
+            return;
+        }
+
+        //include number
+        output.push_back(nums[i]);
+        solve(ans, nums, output, i+1);
+        output.pop_back();                      //backtrack
+
+        while(i+1 < nums.size() && nums[i] == nums[i+1]){
+            i++;
+        }
+        solve(ans, nums, output, i+1);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index = 0;
+
+        //sort first, for skipping duplicates logic
+        sort(nums.begin(), nums.end());
+        solve(ans, nums, output, index);
+
+        return ans;
+    }
+};
