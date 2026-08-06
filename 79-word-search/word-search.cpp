e8369675dda8d41f1f94bbd71cfbe517e341db94
Sanket-Kawale <sanketkawale101@gmail.com>
2026-08-06 19:28:58 +0530

    Time: 731 ms (30.24%) | Memory: 11.7 MB (29.14%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board, string word, int i, int j, int index){
-
-        int row = board.size();
-        int col = board[0].size();
-
-        if(index == word.length()){     //base condition
-            return true;
-        }
-        if(i<0 || i>=row || j<0 || j>=col){     //invalid
-            return false;
-        }
-        if(board[i][j] != word[index]){         //invalid
-            return false;
-        }
-
-        char temp = board[i][j];                //mark curr visited
-        board[i][j] = '#';
-
-        bool found = 
-        solve(board, word, i, j+1, index+1) ||      //right
-        solve(board, word, i, j-1, index+1) ||      //left
-        solve(board, word, i+1, j, index+1) ||      //down
-        solve(board, word, i-1, j, index+1);        //up
-
-        board[i][j] = temp;                      //backtrack
-
-        return found;
-    }
     bool exist(vector<vector<char>>& board, string word) {
-        int row = board.size();
-        int col = board[0].size();
-
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                char temp = word[0];
-
-                if(board[i][j] == temp){
-                    if(solve(board, word, i, j, 0))
+        int m = board.size();
+        int n = board[0].size();
+        
+        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
+            if (k == word.length()) {
+                return true;
+            }
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
+                return false;
+            }
+            
+            char temp = board[i][j];
+            board[i][j] = '\0';
+            
+            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
+                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
+                return true;
+            }
+            
+            board[i][j] = temp; 
+            return false;
+        };
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (backtrack(i, j, 0)) {
                     return true;
                 }
             }
