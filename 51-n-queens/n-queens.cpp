63cb49e5c2b823c7901a2157657cb41607fe5846
Sanket-Kawale <sanketkawale101@gmail.com>
2026-08-08 12:32:10 +0530

    Time: 3 ms (61.48%) | Memory: 10.3 MB (60.09%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,59 +1,41 @@
 class Solution {
-public:
-    bool isSafe(vector<string>board, int row, int col, int n){
-        // three posibilities
-        int dupRow = row;
-        int dupCol = col;
-        //upper diagonal
-        while(row >= 0 && col >= 0){
-            if(board[row][col] == 'Q')    return false;
-            row--;
-            col--;
+private: 
+    bool isSafePlace(int n, vector<string>& nQueens, int row, int col){
+        for(int i=0; i<n; i++){
+            if(nQueens[i][col] == 'Q'){
+                return false;
+            }
         }
-
-        //left side of Q
-        row = dupRow;
-        col = dupCol;
-        while(col >= 0){
-            if(board[row][col] == 'Q')    return false;
-            col--;        
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(nQueens[i][j] == 'Q'){
+                return false;
+            }
         }
-
-        //lower diagonal
-        row = dupRow;
-        col = dupCol;
-        while(row<n && col>=0){
-            if(board[row][col] == 'Q')    return false;
-            row++;
-            col--;
+        for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++){
+            if(nQueens[i][j] == 'Q'){
+                return false;
+            }
         }
-
         return true;
     }
-    void solve(int col, vector<vector<string>> &ans, vector<string>board, int n){
-        if(col == n){
-            ans.push_back(board);
+    void solveNQueens(int n, vector<vector<string>>& output, vector<string>& nQueens, int row){
+        if(row == n){
+            output.push_back(nQueens);
             return;
         }
-
-        for(int row=0; row<n; row++){
-            if(isSafe(board, row, col, n)){         //is it safe to place queen
-                board[row][col] = 'Q';
-                solve(col+1, ans, board, n);
-                board[row][col] = '.';
+        for(int col=0; col<n; col++){
+            if(isSafePlace(n, nQueens, row, col)){
+                nQueens[row][col] = 'Q';
+                solveNQueens(n, output, nQueens, row+1);
+                nQueens[row][col] = '.';
             }
         }
     }
+public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n);        //n*n chess board
-        string s(n, '.');               //initialize string by dummy val & size=n 
-
-        for(int i=0; i<n; i++){
-            board[i] = s;               //all board with '.'
-        }
-
-        solve(0, ans, board, n);        //only colummn needed
-        return ans;
+        vector<vector<string>> output;
+        vector<string> nQueens(n , string(n, '.'));
+        solveNQueens(n, output, nQueens, 0);
+        return output;
     }
 };
