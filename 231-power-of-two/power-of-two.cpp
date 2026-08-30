99acfb37cfa17072aea2f8020a2ff7a9be8c36ae
Sanket-Kawale <sanketkawale101@gmail.com>
2026-08-30 18:33:09 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (12.57%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int ans = 1;
+        // A 32-bit signed integer can have powers of 2 up to 2^30, so we check up to 30.
+
+        for(int i=0; i<=30; i++){
+            ans = pow(2, i);
+
+            if(ans == n)    return true;
+        }
+        return false;
+    }
+};
