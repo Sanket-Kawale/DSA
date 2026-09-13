442269a057ae72f7c3d556bb2b92e5a812e95f01
Sanket-Kawale <sanketkawale101@gmail.com>
2026-09-13 12:49:18 +0530

    Time: 31 ms (86.32%) | Memory: 15.7 MB (44.39%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -2,36 +2,32 @@ class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
-        int ans = 0;
-        for(int row_shift = -n; row_shift < n; row_shift++){
-            for(int col_shift = -n; col_shift < n; col_shift++){
-                vector<vector<int>> temp(n, vector<int>(n, 0));
-                
-                for(int i=0; i<n; i++){
-                    for(int j=0; j<n; j++){
-                        if(img1[i][j] == 1){
-                            int new_i = i + row_shift;
-                            int new_j = j + col_shift;
-                            if(new_i >= 0 && new_i < n && new_j >= 0 && new_j < n) {
-                                temp[new_i][new_j] = 1;
-                            }
-                        }
-                    }
-                }
+        vector<int> bits1, bits2;
 
-                int current_overlap = 0;
-                for(int i=0; i<n; i++){
-                    for(int j=0; j<n; j++){
-                        if(temp[i][j] == 1 && img2[i][j] == 1){
-                            current_overlap = current_overlap + 1;
-                        }
-                    }
-                }
-            
-                ans = max(ans, current_overlap);
+        // 1. Convert 2D coordinates into simple 1D flattened integers
+        // We use a factor of 100 to clearly separate row and column shifts
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (img1[i][j] == 1) bits1.push_back(i * 100 + j);
+                if (img2[i][j] == 1) bits2.push_back(i * 100 + j);
             }
         }
-        
-        return ans;
+
+        // A simple frequency map to count how often each unique shift vector occurs
+        unordered_map<int, int> shift_counts;
+        int max_overlap = 0;
+
+        // 2. Compare every '1' bit position from img1 against img2
+        for (int b1 : bits1) {
+            for (int b2 : bits2) {
+                // The difference between their flattened values represents the precise shift
+                int shift_vector = b2 - b1;
+                
+                shift_counts[shift_vector]++;
+                max_overlap = max(max_overlap, shift_counts[shift_vector]);
+            }
+        }
+
+        return max_overlap;
     }
 };
