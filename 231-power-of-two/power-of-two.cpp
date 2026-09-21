7e56b633aa0830de27f04ccbe430f1bb9efc9e5c
Sanket-Kawale <sanketkawale101@gmail.com>
2026-09-21 19:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (12.73%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        long long ans = 0;
-        for(int i=0; i<=30; i++){
-            ans = pow(2, i);
+        bool ans = 0;
 
-            if(ans == n){
-                return true;
+        for(int i=0; i<=30; i++){
+            long long temp = pow(2,i);
+
+            if(temp == n){
+                ans = 1;
             }
         }
-        return false;
+        return ans;
     }
 };
